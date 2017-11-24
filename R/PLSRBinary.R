48eac5e4b35cf2770a9ee65db5261ae396c5f0a5
Jose Luis Vicente Villardon <villardon@usal.es>
2017-11-24 09:04:37 +0100

    24 Nov

--- R/PLSRBinary.R
+++ R/PLSRBinary.R
@@ -1,5 +1,5 @@
 PLSR1BinFit <- function(Y, X, S=2, InitTransform=5, grouping=NULL, tolerance=0.000005,
-                        maxiter=100, show=FALSE, penalization=0, cte =TRUE)
+                        maxiter=100, show=FALSE, penalization=0, cte =TRUE, Algorithm=1)
 {
   
   if (is.data.frame(X)) X=as.matrix(X)
@@ -19,8 +19,8 @@ PLSR1BinFit <- function(Y, X, S=2, InitTransform=5, grouping=NULL, tolerance=0.0
   J=dim(X)[2]
   
   if (is.numeric(Y)) {Y= as.matrix(Y, I1,1)
-                      rownames(Y)<-rownames(X)
-                      colnames(Y)="Response"}
+  rownames(Y)<-rownames(X)
+  colnames(Y)="Response"}
   I2=dim(Y)[1]
   K=dim(Y)[2]
   inames=rownames(X)
@@ -33,7 +33,7 @@ PLSR1BinFit <- function(Y, X, S=2, InitTransform=5, grouping=NULL, tolerance=0.0
   result$X=X
   result$Y=Y
   result$Initial_Transformation=InitTransform
-
+  
   
   if (!(I1==I2)) stop('The number of rows of both matrices must be the same')
   else I=I1
@@ -62,41 +62,72 @@ PLSR1BinFit <- function(Y, X, S=2, InitTransform=5, grouping=NULL, tolerance=0.0
   freq=matrix(1,I,1)
   w=matrix(0,J,1)
   
-  for (j in 1:J){
-    x=as.matrix(X[,j])
-    colnames(x)=xnames[j]
-    fit=RidgeBinaryLogistic(Y, x)
-    w[j]=fit$beta[2]
-  }
-  w=w/sqrt(sum(w^2))
-  t=X %*% w
-  T[,1]=t
-  W[,1]=w
-  p=t(X) %*% t/ sum(t^2)
-  P[,1]=p
-  X1=X-t %*% t(p)
-  
-  for (i in 2:S){
+  if (Algorithm==1){ 
     for (j in 1:J){
-      x=as.matrix(cbind(T[,1:(i-1)],X[,j]))
-      colnames(x)=c(dimnames[1:(i-1)] ,xnames[j])
+      x=as.matrix(X[,j])
+      colnames(x)=xnames[j]
       fit=RidgeBinaryLogistic(Y, x)
-      w[j]=fit$beta[(i+1)]
+      w[j]=fit$beta[2]
     }
     w=w/sqrt(sum(w^2))
-    t=X1 %*% w
-    T[,i]=t
-    W[,i]=w
+    t=X %*% w
+    T[,1]=t
+    W[,1]=w
     p=t(X) %*% t/ sum(t^2)
-    P[,i]=p
-    X1=X1-t %*% t(p)
+    P[,1]=p
+    X1=X-t %*% t(p)
+    
+    for (i in 2:S){
+      for (j in 1:J){
+        x=as.matrix(cbind(T[,1:(i-1)],X[,j]))
+        colnames(x)=c(dimnames[1:(i-1)] ,xnames[j])
+        fit=RidgeBinaryLogistic(Y, x)
+        w[j]=fit$beta[(i+1)]
+      }
+      w=w/sqrt(sum(w^2))
+      t=X1 %*% w
+      T[,i]=t
+      W[,i]=w
+      p=t(X) %*% t/ sum(t^2)
+      P[,i]=p
+      X1=X1-t %*% t(p)
+    }
+  }
+  
+  if (Algorithm==2){
+    for (i in 1:S){
+      error=1
+      iter=0
+      uini=svd(X)
+      u=runif(I)
+      while ((error>tolerance) & (iter<maxiter)){
+        iter=iter+1
+        w=(t(X) %*% u)/sum(u^2)
+        w=w/sqrt(sum(w^2))
+        t=X %*% w
+        fit=RidgeBinaryLogistic(Y, t, penalization=penalization, cte=cte)
+        if (cte) c=fit$beta[2]
+        else c=fit$beta[1]
+        newu= fit$linterm
+        error=sum((u-newu)^2)
+        u=newu
+        if (show) print(c(i, iter, error))
+      }
+      T[,i]=t
+      U[,i]=u
+      W[,i]=w
+      C[,i]=c
+      p=t(X) %*% t/ sum(t^2)
+      P[,i]=p
+      X=X-t %*% t(p)
+    }
   }
   
   fit=RidgeBinaryLogistic(Y, T, penalization=penalization, cte=cte)
   
   rownames(P)=xnames
   colnames(P)=dimnames
-
+  
   result$XScores=T
   result$XWeights=W
   result$XLoadings=P
