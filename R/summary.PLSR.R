4029ede57eb2b75811373d6157d868f670df0f3c
villardon <villardon@usal.es>
2018-01-10 11:09:42 +0100

    todo10

--- R/summary.PLSR.R
+++ R/summary.PLSR.R
@@ -1,3 +1,15 @@
 summary.PLSR <- function(plsr){
-  
+  cat(" ###### PLS Regression with Continuous Responses #######\n\n")
+  cat("________________________________________________\n\n")
+  cat("Method\n")
+  print(x$Method)
+  cat("Initial transformation of X : \n")
+  print(x$Initial_Transformation)
+  cat("________________________________________________\n\n")
+  cat("Correlations of the X variables and the PLS Componentes \n")
+  print(round(x$XStructure, digits=3))
+  cat("Contributions of the the PLS Componentes to the X variables  \n")
+  Contrib=cbind(x$XStructure^2, apply(x$XStructure^2,1,sum))
+  colnames(Contrib)=c(colnames(x$XStructure), "  Total")
+  print(round(Contrib, digits=3))
 }
