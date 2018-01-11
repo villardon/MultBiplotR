8d9ab02234c9791a697acfc3638ffbc3b149b23c
Jose Luis Vicente Villardon <villardon@usal.es>
2018-01-11 14:36:47 +0100

    11.1

--- R/HJ.Biplot.R
+++ R/HJ.Biplot.R
@@ -1,4 +1,4 @@
-HJ.Biplot <- function(X, dimension = 3, Scaling = 4, sup.rows = NULL, sup.cols = NULL, grouping=NULL){
+HJ.Biplot <- function(X, dimension = 3, Scaling = 5, sup.rows = NULL, sup.cols = NULL, grouping=NULL){
   bip=PCA.Biplot(X, alpha = 2, dimension = dimension, Scaling = Scaling, sup.rows = sup.rows, sup.cols = sup.cols, grouping=grouping)
   bip$Type = "HJ"
   bip$Title="HJ Biplot"
